4f05e7b9ad00df488a5792bee332c3f3893ecf2f
maiha <maiha@wota.jp>
2016-06-27 09:57:16 +0900

    version: 0.1.5

--- src/bin/rcm.cr
+++ src/bin/rcm.cr
@@ -15,7 +15,7 @@ class Rcm::Main
   option help  : Bool  , "--help", "Output this help and exit", false
   
   usage <<-EOF
-    #{$0} version 0.1.4
+    #{$0} version 0.1.5
 
     Usage: #{$0} <commands>
 
