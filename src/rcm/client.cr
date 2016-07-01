bb7637214b3fb96a16157b3946def0b62e0d0043
maiha <maiha@wota.jp>
2016-07-01 20:04:05 +0900

    Rcm::Client accepts bootstrap info

--- src/rcm/client.cr
+++ src/rcm/client.cr
@@ -1,9 +1,30 @@
 require "./commands"
 
+# TODO: rename to Redis::Cluster
 class Rcm::Client
+  def self.load_info(addrs : Array(Addr), pass = nil) : ClusterInfo
+    addrs.each do |addr|
+      redis = nil
+      begin
+        redis = Redis.new(addr.host, addr.port, pass)
+        return ClusterInfo.parse(redis.not_nil!.nodes)
+      rescue
+      ensure
+        redis.try(&.close)
+      end
+    end
+    raise "Redis not found: #{addrs.map(&.to_s).inspect}"
+  end
+
   property cluster_info, node2redis
   delegate nodes, to: @cluster_info
-  
+
+  def initialize(bootstrap : String, @password : String? = nil)
+    addrs = bootstrap.split(",").map{|s| Addr.parse(s)}
+    info  = self.class.load_info(addrs, @password)
+    initialize(info, @password)
+  end
+
   def initialize(@cluster_info : ClusterInfo, @password : String? = nil)
     @slot2nodes = @cluster_info.slot2nodes.as(Hash(Int32, NodeInfo))
     @node2redis = Hash(NodeInfo, Redis).new
@@ -21,9 +42,12 @@ class Rcm::Client
   end
 
   def redis(key : String)
+    redis(node(key))
+  end
+
+  def node(key : String)
     slot = Rcm.slot(key)
-    node = @slot2nodes.fetch(slot) { raise "This cluster doesn't cover slot=#{slot} (key=#{key.inspect})" }
-    redis(node)
+    return @slot2nodes.fetch(slot) { raise "This cluster doesn't cover slot=#{slot} (key=#{key.inspect})" }
   end
 
   def each
