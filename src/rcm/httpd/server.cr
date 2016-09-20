d44ca9c2d5475c98136a229105b81545d1f0fb07
maiha <maiha@wota.jp>
2016-09-20 17:45:25 +0900

    httpd: fix listen port

--- src/rcm/httpd/server.cr
+++ src/rcm/httpd/server.cr
@@ -2,13 +2,17 @@ require "http/server"
 
 module Rcm::Httpd
   class Server
+    delegate config, to: Kemal
+    
     def initialize(@redis : ::Redis::Client, @listen : Bootstrap)
-      Kemal.config.add_handler RedisHandler.new(@redis, watch_interval: 1.second)
+      config.add_handler RedisHandler.new(@redis, watch_interval: 1.second)
       pass = @redis.password || @listen.pass
       if pass
         auth_handler = Kemal::Middleware::HTTPBasicAuth.new("redis", pass)
-        Kemal.config.add_handler auth_handler
+        config.add_handler auth_handler
       end
+
+      config.port = @listen.port
     end
     
     def start
