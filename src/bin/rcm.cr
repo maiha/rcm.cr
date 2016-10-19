5526d124d5bdde5780b1c85dc83204d1edb59b1e
maiha <maiha@wota.jp>
2016-10-19 23:21:46 +0900

    version: 0.6.2

--- src/bin/rcm.cr
+++ src/bin/rcm.cr
@@ -5,7 +5,7 @@ require "colorize"
 class Rcm::Main
   include Options
 
-  VERSION = "0.6.1"
+  VERSION = "0.6.2"
 
   option uri   : String?, "-u <uri>", "Give host,port,pass at once by 'pass@host:port'", nil
   option host  : String?, "-h <hostname>", "Server hostname (override uri)", nil
