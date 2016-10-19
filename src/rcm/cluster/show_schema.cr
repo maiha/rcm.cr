1bdfe496b7d2ea268b38a6aa3f0320175751ab37
maiha <maiha@wota.jp>
2016-10-19 11:09:09 +0900

    schema: sort slaves by port and host

--- src/rcm/cluster/show_schema.cr
+++ src/rcm/cluster/show_schema.cr
@@ -17,9 +17,19 @@ module Rcm::Cluster
     private def show_schema(io : IO)
       info.each_serving_masters_with_slaves do |master, slaves|
         slot = "[%-11s]" % master.slot
-        node = ([master] + slaves).map(&.addr.to_s).join(" ")
+        node = ([master] + sort_slaves(slaves)).map(&.addr.to_s).join(" ")
         io.puts "%s %s" % [slot, node]
       end
+    end
+
+    # sort slaves by port and host for durability pretty printing
+    private def sort_slaves(nodes)
+      nodes.sort {|a,b|
+        case a.port <=> b.port
+        when 0 then a.host <=> b.host
+        else a.port <=> b.port
+        end
+      }
     end
   end
 end
