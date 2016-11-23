b6693f874ed47e7ddfe723b3c4b28949915ddc67
maiha <maiha@wota.jp>
2016-11-23 22:33:05 +0900

    version: 0.6.6

--- src/bin/rcm.cr
+++ src/bin/rcm.cr
@@ -6,7 +6,7 @@ class Rcm::Main
   include Opts
   include Rcm::Cluster::NodesHelper
 
-  VERSION = "0.6.5"
+  VERSION = "0.6.6"
 
   option uri   : String?, "-u <uri>", "Give host,port,pass at once by 'pass@host:port'", nil
   option host  : String?, "-h <hostname>", "Server hostname (override uri)", nil
